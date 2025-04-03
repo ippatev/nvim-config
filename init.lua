922043dc3281c85305ead900849145639076a206
Aleksandr Ippatev <alexanderippatiev24@gmail.com>
2025-04-03 15:20:15 +0900

    feat: add new cmp plugins

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 require("sg").setup({
-  enable_cody = false,
+  enable_cody = true,
   lsp = {
     preload_files = false,
   },
