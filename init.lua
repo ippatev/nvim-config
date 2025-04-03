475a190e923029fa09505dd07712e2cc7e3f3bc9
Aleksandr Ippatev <alexanderippatiev24@gmail.com>
2025-04-03 16:41:06 +0900

    feat: update sg plugin setup configs

--- init.lua
+++ init.lua
@@ -2,7 +2,11 @@
 require("config.lazy")
 require("sg").setup({
   enable_cody = true,
+  accept_tos = true,
+  -- download_blnaries = true,
   lsp = {
     preload_files = false,
   },
+  endpoint = "https://sourcegraph.com",
+  token = "sgp_fd1b4edb60bf82b8_43181da1aaa4ca815d59ef68f97372cd65e857b0",
 })
