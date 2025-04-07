b67ce0ce1307119f4c151e572966ee6df1e1fff3
Aleksandr Ippatev <alexanderippatiev24@gmail.com>
2025-04-07 11:50:41 +0900

    feat: update cody configs

--- init.lua
+++ init.lua
@@ -1,12 +1,10 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 require("sg").setup({
-  enable_cody = true,
-  accept_tos = true,
-  -- download_blnaries = true,
+  enable_cody = false,
   lsp = {
     preload_files = false,
   },
-  endpoint = "https://sourcegraph.com",
+  endpoint = "https://sourcegraph.com/",
   token = os.getenv("SOURCEGRAPH_TOKEN"),
 })
