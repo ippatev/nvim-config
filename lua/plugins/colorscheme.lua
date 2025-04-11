57791a1deca78d8f97b0ba7abc94be8f2181e3a1
Aleksandr Ippatev <alexanderippatiev24@gmail.com>
2025-04-11 10:34:29 +0900

    feat: update colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,11 +1,11 @@
 return {
   -- { "ellisonleao/gruvbox.nvim" },
-  -- { "miikanissi/modus-themes.nvim", priority = 1000 },
-  {"vague2k/vague.nvim"},
+  { "miikanissi/modus-themes.nvim", priority = 1000 },
+  -- {"vague2k/vague.nvim"},
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "vague",
+      colorscheme = "modus",
     },
   },
 }
