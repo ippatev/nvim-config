0d16c01be602589233fbb1fc720c5bfabf9eed01
Aleksandr Ippatev <alexanderippatiev24@gmail.com>
2025-06-17 18:22:09 +0900

    update(keymaps): <leader>e - toggle mini.files

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,14 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+-- Удаляем стандартный keymap <Space>e
+vim.keymap.del("n", "<leader>e", { silent = true })
+
+-- Toggle для mini.files
+vim.keymap.set("n", "<leader>e", function()
+  local mf = require("mini.files")
+  if not mf.close() then
+    mf.open(vim.api.nvim_buf_get_name(0), true)
+  end
+end, { desc = "Toggle mini.files" })
