690945d0be48109ebd61090b91eb6e7062634c98
Aleksandr Ippatev <alexanderippatiev24@gmail.com>
2025-06-17 18:40:56 +0900

    fix: disable netrw integration neo-tree

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,9 @@
+   return {
+     "nvim-neo-tree/neo-tree.nvim",
+     opts = {
+       filesystem = {
+         hijack_netrw_behavior = "disabled", -- Отключает перехват netrw
+       },
+     },
+   }
+   
