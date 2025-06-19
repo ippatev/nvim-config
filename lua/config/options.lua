63b07df7340457e0f60a49f594cac018a2077eac
Aleksandr Ippatev <alexanderippatiev24@gmail.com>
2025-06-19 11:26:17 +0900

    fix: fzf default command env

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,5 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.autoformat = false
-vim.opt.clipboard = 'unnamedplus'
+vim.opt.clipboard = "unnamedplus"
 vim.g.lazyvim_prettier_needs_config = false
+vim.env.FZF_DEFAULT_COMMAND = 'rg --files --hidden --follow --no-ignore-vcs -g "!{.git,node_modules}"'
