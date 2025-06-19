91d72eaddf4d4090eb008035411ad110375388d6
Aleksandr Ippatev <alexanderippatiev24@gmail.com>
2025-06-19 11:25:46 +0900

    update: mini.files mappings

---
+++ lua/plugins/mini-files.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "echasnovski/mini.files",
+    opts = {
+      mappings = {
+        go_in = "l",
+        go_out = "h",
+        create = "o",
+        synchronize = "<CR>",
+        close = "<Esc>",
+      },
+    },
+  },
+}
