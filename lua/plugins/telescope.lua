9f412b74df3e7bea74af7efbb227c1f80bea3f3b
Aleksandr Ippatev <alexanderippatiev24@gmail.com>
2025-06-19 11:25:33 +0900

    update: telescope fzf

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,19 +4,24 @@ return {
     tag = "0.1.8",
     dependencies = {
       "nvim-lua/plenary.nvim",
-      { "nvim-telescope/telescope-fzf-native.nvim", build  = "make"},
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     },
     config = function()
-      require("telescope").setup {
+      require("telescope").setup({
+        defaults = {
+          file_ignore_patterns = { "%.git/.*", "node_modules/.*" },
+        },
         pickers = {
           find_files = {
-            theme = "ivy"
-          }
-        }
-      }
+            -- theme = "ivy",
+            hidden = true,
+            no_ignore = false,
+          },
+        },
+      })
 
       vim.keymap.set("n", "<space>fh", require("telescope.builtin").help_tags)
       vim.keymap.set("n", "<space>fd", require("telescope.builtin").find_files)
-    end
-  }
+    end,
+  },
 }
