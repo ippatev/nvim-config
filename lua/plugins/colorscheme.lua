93bfb187da49adc5c3aca4df76b05beccc6dae9d
Aleksandr Ippatev <alexanderippatiev24@gmail.com>
2025-07-17 11:12:22 +0900

    update: colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,19 @@
 return {
   -- { "ellisonleao/gruvbox.nvim" },
-  { "miikanissi/modus-themes.nvim", priority = 1000 },
+  -- { "miikanissi/modus-themes.nvim", priority = 1000 },
   -- {"vague2k/vague.nvim"},
   -- { "bettervim/yugen.nvim" },
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "modus",
+  --   },
+  -- },
   {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "modus",
-    },
+    "adibhanna/forest-night.nvim",
+    priority = 1000,
+    config = function()
+      vim.cmd([[colorscheme forest-night]])
+    end,
   },
 }
